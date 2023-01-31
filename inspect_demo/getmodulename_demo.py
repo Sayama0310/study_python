dbac2e5f54ae6c4e0f068f120e3921799f710af0
Sayama0310 <montblanc3060@gmail.com>
2023-01-31 22:26:19 +0900

    [inspect] getmodulenameデモでassertを使うように変更

--- inspect_demo/getmodulename_demo.py
+++ inspect_demo/getmodulename_demo.py
@@ -1,29 +1,25 @@
 import inspect
 
 # pythonモジュールとして適切なパスに対して、そのモジュール名を返却します。
-# pythonモジュールとして適切かどうかはファイルの拡張子で判断します。またその拡張子のリストは importlib.machinery.all_suffixes() 
+# pythonモジュールとして適切かどうかはファイルの拡張子で判断します。またその拡張子のリストは importlib.machinery.all_suffixes()
 # によって確認できます。
 
 # 実際に存在するPythonモジュールまでのパス
 path = r'/Users/sayama_yusei/Documents/private/study_python/inspect_demo/getmodulename_demo.py'
-print(inspect.getmodulename(path=path))  # getmodulename_demo
-
+assert inspect.getmodulename(path=path) == 'getmodulename_demo'
 
 # 実際には存在しないが、Pythonモジュールとしては妥当な名前を持つパス (拡張子が.py)
 path = r'/Users/sayama_yusei/Documents/private/study_python/inspect_demo/does_not_exist.py'
-print(inspect.getmodulename(path=path))  # does_not_exist
-
+assert inspect.getmodulename(path=path) == 'does_not_exist'
 
 # 実際には存在しないが、Pythonモジュールとしては妥当な名前を持つパス (拡張子が.so)
 path = r'/Users/sayama_yusei/Documents/private/study_python/inspect_demo/special_extension.so'
-print(inspect.getmodulename(path=path))  # special_extension
-
+assert inspect.getmodulename(path=path) == 'special_extension'
 
 # Pythonモジュールとしては妥当でない名前を持つパス (拡張子が.md)
 path = r'/Users/sayama_yusei/Documents/private/study_python/README.md'
-print(inspect.getmodulename(path=path))  # None
-
+assert inspect.getmodulename(path=path) == None
 
 # Pythonパッケージまでのパス
 path = r'/Users/sayama_yusei/Documents/private/study_python/inspect_demo'
-print(inspect.getmodulename(path=path))  # None
+assert inspect.getmodulename(path=path) == None
